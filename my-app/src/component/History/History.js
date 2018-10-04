06efb082d33fbffe3e2bc4dff59a070fc5371f1d
AshishGoyal <goyal.ashish3@gmail.com>
2018-10-04 14:56:42 +0530

    replaced binding in history js with arrow functions

--- my-app/src/component/History/History.js
+++ my-app/src/component/History/History.js
@@ -13,20 +13,20 @@ import React, { Component } from 'react';
               threedaysprice: {},
               fourdaysprice: {}
           }
-          this.getBTCPrices = this.getBTCPrices.bind(this);
-          this.getETHPrices = this.getETHPrices.bind(this);
-          this.getLTCPrices = this.getLTCPrices.bind(this);
+        //   this.getBTCPrices = this.getBTCPrices.bind(this);
+        //   this.getETHPrices = this.getETHPrices.bind(this);
+        //   this.getLTCPrices = this.getLTCPrices.bind(this);
         }
         // This function gets the ETH price for a specific timestamp/date. The date is passed in as an argument
-        getETHPrices (date) {
+        getETHPrices =(date)=> {
             return axios.get('https://min-api.cryptocompare.com/data/pricehistorical?fsym=ETH&tsyms=USD&ts=' + date);
         }
         // This function gets the BTC price for a specific timestamp/date. The date is passed in as an argument
-        getBTCPrices (date) {
+        getBTCPrices =(date)=> {
             return axios.get('https://min-api.cryptocompare.com/data/pricehistorical?fsym=BTC&tsyms=USD&ts=' + date);
         }
         // This function gets the LTC price for a specific timestamp/date. The date is passed in as an argument
-        getLTCPrices (date) {
+        getLTCPrices =(date)=> {
             return axios.get('https://min-api.cryptocompare.com/data/pricehistorical?fsym=LTC&tsyms=USD&ts=' + date);
         }
 
